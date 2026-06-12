42aa9f11ed1babc4fd7239e77cffdd89ea7f5386
junayedislamtamim <junayedislamtamim8@gmail.com>
2026-06-12 22:26:38 +0600

    Temperature conversion using js
    Convert farenheit to celcius and celcius to farenheit using js

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemperature) {
+ return Math.round((farenheitTemperature - 32) * (5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusTemperature) {
+  return Math.round(((celciusTemperature * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
